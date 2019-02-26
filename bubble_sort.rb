762a2f724c134b8bcce1c39f88d04a16526dba84
mumani <mumelvis39@gmail.com>
2019-02-26 22:07:58 +0300

    Bubble Sort Method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,16 @@
+def bubbleSort(arr)
+   for i in 0 ... (arr.length - 1)
+       swapped = false
+       for j in 0 ... (arr.length - i - 1)
+           if arr[j] > arr[j + 1]
+               arr[j], arr[j + 1] = arr[j + 1], arr[j]
+               swapped = true
+           end
+       end
+       break if swapped == false
+   end
+end
 
+nums = [3, 2, 1, 5, 21, 8, 6, 9, 4, 7]
+bubbleSort(nums)
+p nums
