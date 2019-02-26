3df1e2b7743e977c256b221ade7eca0c4c8e4b65
newcoder <newcoder17@gmail.com>
2019-02-26 22:40:40 +0100

    with yield

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,34 @@
-def bubbleSort(arr)
-   for i in 0 ... (arr.length - 1)
-       swapped = false
-       for j in 0 ... (arr.length - i - 1)
-           if arr[j] > arr[j + 1]
-               arr[j], arr[j + 1] = arr[j + 1], arr[j]
-               swapped = true
-           end
-       end
-       break if swapped == false
-   end
-end
+class BubbleSort
+  def self.bubbleSort(arr)
+     for i in 0 ... (arr.length - 1)
+         swapped = false
+         for j in 0 ... (arr.length - i - 1)
+             if arr[j] > arr[j + 1]
+                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                 swapped = true
+             end
+         end
+         break if swapped == false
+     end
+     arr
+  end
 
+  def self.bubble_sort_by(arr)
+    for i in 0 ... (arr.length - 1)
+      swapped = false
+      for j in 0 ... (arr.length - i - 1)
+        if yield(arr[j], arr[j+1]) > 0
+          arr[j], arr[j + 1] = arr[j + 1], arr[j]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    puts arr
+  end
+end
 nums = [3, 2, 1, 5, 21, 8, 6, 9, 4, 7]
-bubbleSort(nums)
+puts BubbleSort.bubbleSort(nums)
 p nums
+
+puts BubbleSort.bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}
